9d3246310d111ec5ca52006bab2b3b7c3893a5f5
TEST <test@test.t>
2019-12-24 12:38:43 +0000

    cleanup

--- check_virtual_memory_structure.c
+++ check_virtual_memory_structure.c
@@ -2,11 +2,10 @@
 #include <stdlib.h>
 #include "libkdump.h"
 
-#define RED     "\033[31m"
-#define YELLOW  "\033[33m"
-#define GREEN   "\033[32m"
-#define BLUE    "\033[34;1m"
-#define RESET   "\033[0m"
+#define R "\033[31m"
+#define G "\033[32m"
+#define B "\033[34;1m"
+#define E "\033[0m"
 
 int data = 5555;
 int bss = 0;
@@ -15,8 +14,8 @@ int bss2;
 void text(){ 1+1 == 2; }
 
 int main(int argc, char * argv[]){
-    if (argc < 2) {
-        fputs(RED "Please pass some arguments...\n" RESET , stderr);
+    if (argc != 3) {
+        fputs(R "Please pass TWO arguments...\n" E , stderr);
         return 1;
     }
     static char * data2 = "twice";
@@ -26,38 +25,45 @@ int main(int argc, char * argv[]){
     int * heap2 = (int *)malloc(sizeof(int));
     char * environment_variable = getenv("PATH");
 
-    printf(YELLOW "Let's check address of each memory section from BOTTOM to TOP.\n" RESET);
-    printf( GREEN "Text  section " RESET " : " BLUE "%p\n", text);
-    printf( GREEN "Data  section " RESET " : " BLUE "%p\n", &data);
-    printf( GREEN "              " RESET " : " BLUE "%p\n", &data2);
-    printf( GREEN "BSS   section " RESET " : " BLUE "%p\n", &bss);
-    printf( GREEN "              " RESET " : " BLUE "%p\n", &bss2);
-    printf( GREEN "Heap  section " RESET " : " BLUE "%p\n", heap);
-    printf( GREEN "              " RESET " : " BLUE "%p\n", heap2);
-    printf( GREEN "Stack section " RESET " : " BLUE "%p\n", &stack);
-    printf( GREEN "              " RESET " : " BLUE "%p\n", &stack2);
-    printf( GREEN "Main arg-argc " RESET " : " BLUE "%p\n", &argc);
-    printf( GREEN "     arg-argv " RESET " : " BLUE "%p\n", argv);
-    printf( GREEN "     argv[1]  " RESET " : " BLUE "%p\n", argv[1]);
-    printf( GREEN "Environment   " RESET " : " BLUE "%p\n", environment_variable);
-    printf(RESET "All of these are Virtual Address\n\n");
+    printf("Let's check addresses of each memory section of virtual memory system.\n");
+    printf(G"Text  section " E " : " B "%p\n", text);
+    printf(G"Data  section " E " : " B "%p\n", &data2);
+    printf(G"              " E " : " B "%p\n", &data);
+    printf(G"BSS   section " E " : " B "%p\n", &bss);
+    printf(G"              " E " : " B "%p\n", &bss2);
+    printf(G"Heap  section " E " : " B "%p\n", heap);
+    printf(G"              " E " : " B "%p\n", heap2);
+    printf(G"Main arg-argc " E " : " B "%p\n", &argc);
+    printf(G"Stack section " E " : " B "%p\n", &stack);
+    printf(G"              " E " : " B "%p\n", &stack2);
+    printf(G"Main arg-argv " E " : " B "%p\n", argv);
+    printf(G"     argv[1]  " E " : " B "%p\n", argv[1]);
+    printf(G"     argv[2]  " E " : " B "%p\n", argv[2]);
+    printf(G"Env variable  " E " : " B "%p\n", environment_variable);
+    printf(R "These are Virtual Address\n\n"E);
 
-    printf("Let's check physical address(Real Address)!\n");
+    printf("Let's check physical addresses(Real Address)!\n");
     void * physical_text = libkdump_virt_to_phys((size_t)text);
     if (physical_text == NULL) {
-        puts("\x1b[31mIf you want to see physical memory address, reexecute with root privileges!");
+        puts(R "If you want to see physical memory address, reexecute with root privileges!"E);
         return 1;
     }
-    printf("Text  section : %p\n", physical_text);
-    printf("Data  section : %p\n", libkdump_virt_to_phys((size_t)&data));
-    printf("              : %p\n", libkdump_virt_to_phys((size_t)&data2));
-    printf("BSS   section : %p\n", libkdump_virt_to_phys((size_t)&bss));
-    printf("              : %p\n", libkdump_virt_to_phys((size_t)&bss2));
-    printf("Heap  section : %p\n", libkdump_virt_to_phys((size_t)heap));
-    printf("              : %p\n", libkdump_virt_to_phys((size_t)heap2));
-    printf("Stack section : %p\n", libkdump_virt_to_phys((size_t)&stack));
-    printf("              : %p\n", libkdump_virt_to_phys((size_t)&stack2));
-    printf("Main argument : %p\n", libkdump_virt_to_phys((size_t)argv));
-    printf("Environment   : %p\n", libkdump_virt_to_phys((size_t)environment_variable));
+    printf(G"Text  section " E " : " B "%p\n", physical_text);
+    printf(G"Data  section " E " : " B "%p\n", libkdump_virt_to_phys((size_t)&data2));
+    printf(G"              " E " : " B "%p\n", libkdump_virt_to_phys((size_t)&data));
+    printf(G"BSS   section " E " : " B "%p\n", libkdump_virt_to_phys((size_t)&bss));
+    printf(G"              " E " : " B "%p\n", libkdump_virt_to_phys((size_t)&bss2));
+    printf(G"Heap  section " E " : " B "%p\n", libkdump_virt_to_phys((size_t)heap));
+    printf(G"              " E " : " B "%p\n", libkdump_virt_to_phys((size_t)heap2));
+    printf(G"Main arg-argc " E " : " B "%p\n", libkdump_virt_to_phys((size_t)&argc));
+    printf(G"Stack section " E " : " B "%p\n", libkdump_virt_to_phys((size_t)&stack));
+    printf(G"              " E " : " B "%p\n", libkdump_virt_to_phys((size_t)&stack2));
+    printf(G"Main argument " E " : " B "%p\n", libkdump_virt_to_phys((size_t)argv));
+    printf(G"     arg-argv " E " : " B "%p\n", libkdump_virt_to_phys((size_t)argv));
+    printf(G"     argv[1]  " E " : " B "%p\n", libkdump_virt_to_phys((size_t)argv[1]));
+    printf(G"     argv[2]  " E " : " B "%p\n", libkdump_virt_to_phys((size_t)argv[2]));
+    printf(G"Env variable  " E " : " B "%p\n", libkdump_virt_to_phys((size_t)environment_variable));
+    printf(R "These are real Physical Address\n"E);
     return 0;
 }
+
